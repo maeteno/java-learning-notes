345a20edd14fd70159895e2fbd52b9998ab731fb
alan-fu <ssisoo@live.cn>
2020-08-06 17:07:24 +0800

    feat(notes): 更新lambda笔记

--- src/com/maeteno/jss/lambda/Main.java
+++ src/com/maeteno/jss/lambda/Main.java
@@ -3,12 +3,23 @@ package com.maeteno.jss.lambda;
 import java.util.function.Predicate;
 
 public class Main {
-
+    
     public static void main(String[] args){
-        lambda((String a) -> {System.out.println(a);return false;});
+        Predicate<String> p = (String a) -> {System.out.println(a);return false;};
+        lambda(new Predicate<String>(){
+            public boolean test(String s){
+                return true;
+            }
+        });
     }
 
     public static void lambda(Predicate<String> p){
         p.test("12233");
+    }
+
+    class Demo implements Predicate<String> {
+        public boolean test(String t){
+            return false;
+        }
     }
 }
