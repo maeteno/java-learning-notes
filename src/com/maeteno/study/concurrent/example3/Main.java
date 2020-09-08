e4ac993a91d6261b256c0e2448324014bdb71489
Alan.Fu <ssisoo@live.cn>
2020-09-08 20:30:13 +0800

    feat(future): 新增线程池执行future

--- src/com/maeteno/study/concurrent/example3/Main.java
+++ src/com/maeteno/study/concurrent/example3/Main.java
@@ -3,18 +3,25 @@ package com.maeteno.study.concurrent.example3;
 import java.awt.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Callable c = new CallableImpl();
-        FutureTask<Integer> f = new FutureTask<>(c);
 
+        FutureTask<Integer> f = new FutureTask<>(c);
         new Thread(f).start();
         System.out.println("Main 1");
-
         var result = f.get();
         System.out.println(result);
+
+        ExecutorService executor =  Executors.newFixedThreadPool(10);
+        Future f2 = executor.submit(c);
+        var result2 = f2.get();
+        System.out.println(result2);
 
         System.out.println("Main End");
     }
