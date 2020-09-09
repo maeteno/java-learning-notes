9b4e9b10841723d5c8f6ee015573b1f0194d049c
alan-fu <ssisoo@live.cn>
2020-09-09 12:59:16 +0800

    feat(concurrent): 线程池示例代码

--- src/com/maeteno/study/concurrent/example4/Main.java
+++ src/com/maeteno/study/concurrent/example4/Main.java
@@ -1,23 +1,35 @@
 package com.maeteno.study.concurrent.example4;
 
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
     public static void main(String[] args) {
-        var pool = new ThreadPoolExecutor(5, 10, 10L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+        var pool = new ThreadPoolExecutor(50, 100, 10L, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
+        // var pool2 = Executors.newFixedThreadPool(10);
+
+        var sum = new AtomicInteger();
 
         var index = 0;
-        while (index++ < 10) {
-            pool.execute(() -> System.out.println("Pool"));
+        while (index++ < 1000) {
+            pool.execute(() -> {
+                System.out.println("Pool: " + sum.addAndGet(1));
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println("ERROR");
+                    e.printStackTrace();
+                }
+            });
         }
 
-       var count =  pool.getActiveCount();
+        var count = pool.getActiveCount();
 
         System.out.println(count);
 
         pool.shutdown();
+
+        System.out.println("END");
     }
 }
