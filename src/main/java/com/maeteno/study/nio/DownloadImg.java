ba14f75c834d1fb269179a0eb7be818b96462d3a
傅双亮 <shuangliang.fu@baozun.com>
2021-01-28 10:47:05 +0800

    feat(nio): 图片下载

--- src/main/java/com/maeteno/study/nio/DownloadImg.java
+++ src/main/java/com/maeteno/study/nio/DownloadImg.java
@@ -1,5 +1,7 @@
 package com.maeteno.study.nio;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.ByteArrayOutputStream;
@@ -18,7 +20,18 @@ import java.util.Set;
 
 @Slf4j
 public class DownloadImg {
+
+    @Builder
+    @Data
+    public static class Info {
+        private String path;
+        private String name;
+        private boolean isSend;
+    }
+
     public static void main(String[] args) throws IOException {
+        long start = System.currentTimeMillis();
+
         URL u = new URL("http://i1.ygimg.cn");
         String host = u.getHost();
         int port = 80;
@@ -26,55 +39,78 @@ public class DownloadImg {
 
         Selector selector = Selector.open();
 
-        String path = "/pics/adidasneo/2019/101231471/101231471_01_f.png?2";
-        SocketChannel socketChannel1 = SocketChannel.open(remote);
-        socketChannel1.configureBlocking(false);
-        socketChannel1.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+        String path1 = "/pics/adidasneo/2019/101231471/101231471_01_f.png?2";
+        String path2 = "/pics/adidasneo/2019/101231471/101231471_01_001_f.png?2";
 
-        while (selector.select() > 0) {
+        int index = 0;
+        while (index++ < 100) {
+            SocketChannel socketChannel = SocketChannel.open(remote);
+            socketChannel.configureBlocking(false);
+            SelectionKey selectionKey = socketChannel.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+
+            String path = path1;
+//            if (index % 5 == 0) {
+//                path = path2;
+//            }
+
+            selectionKey.attach(
+                    Info.builder()
+                            .path(path)
+                            .name("00" + index + ".png")
+                            .isSend(false)
+                            .build()
+            );
+        }
+
+        while (selector.select(10L) > 0) {
             Set<SelectionKey> selectedKeys = selector.selectedKeys();
             Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
             while (keyIterator.hasNext()) {
                 SelectionKey key = keyIterator.next();
                 SocketChannel socketChannel = (SocketChannel) key.channel();
+                Info info = (Info) key.attachment();
+                log.info("Info:{}", info);
 
-                if (key.isAcceptable()) {
-                    log.info("isAcceptable");
-                }
-                if (key.isConnectable()) {
-                    log.info("isConnectable");
-                }
+                if (info.isSend() && key.isReadable()) {
+                    log.info("isReadable");
 
-                if (key.isReadable()) {
                     ByteBuffer buffer = ByteBuffer.allocate(8192);
                     ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-                    FileOutputStream out = new FileOutputStream("101231471_01_f.png");
+                    FileOutputStream out = new FileOutputStream(info.getName());
 
-                    // 根据连续换行符截取消息体
-                    int tag = 0;
-                    while (socketChannel.read(buffer) != -1) {
-                        buffer.flip();
-                        while (buffer.hasRemaining()) {
-                            byte b = buffer.get();
-                            if (tag >= 4) {
-                                byteOut.write(b);
-                            } else {
-                                if (b == '\r' || b == '\n') {
-                                    tag += 1;
+                    try {
+                        // 根据连续换行符截取消息体
+                        int tag = 0;
+                        while (socketChannel.read(buffer) != -1) {
+                            buffer.flip();
+                            while (buffer.hasRemaining()) {
+                                byte b = buffer.get();
+                                if (tag >= 4) {
+                                    byteOut.write(b);
                                 } else {
-                                    tag = 0;
+                                    if (b == '\r' || b == '\n') {
+                                        tag += 1;
+                                    } else {
+                                        tag = 0;
+                                    }
                                 }
                             }
+
+                            buffer.clear();
                         }
-
-                        buffer.clear();
+                        out.write(byteOut.toByteArray());
+                    } catch (IOException exception) {
+                        log.error("图片下载失败：", exception);
+                    } finally {
+                        out.close();
+                        socketChannel.close();
                     }
+                }
 
-                    out.write(byteOut.toByteArray());
-                    out.close();
-                    socketChannel.close();
-                } else if (key.isWritable()) {
-                    String request = "GET " + path + " HTTP/1.1\r\n"
+                if (!info.isSend() && key.isWritable()) {
+                    log.info("isWritable");
+
+                    String request = "GET " + info.getPath() + " HTTP/1.1\r\n"
                             + "Host: " + host + "\r\n"
                             + "Accept: image/*\r\n"
                             + "Connection: close\r\n"
@@ -83,6 +119,7 @@ public class DownloadImg {
 
                     ByteBuffer header = ByteBuffer.wrap(request.getBytes(StandardCharsets.US_ASCII));
                     socketChannel.write(header);
+                    info.setSend(true);
                 }
 
                 keyIterator.remove();
@@ -90,5 +127,9 @@ public class DownloadImg {
         }
 
         selector.close();
+
+        long time = System.currentTimeMillis() - start;
+
+        log.info("Time: {} s", time / 1000);
     }
 }
